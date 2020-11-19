d6dbc4257fa6729df3c8f94a574075c5bcde0688
Allen <howlmoon123@gmail.com>
2020-11-19 05:45:26 -0600

    OnSelectedChange
    Callbacks

--- Assets/Scripts/Dialog/Editor/DialogEditor.cs
+++ Assets/Scripts/Dialog/Editor/DialogEditor.cs
@@ -8,13 +8,15 @@ namespace Dialog.Editor
 {
     public class DialogEditor : EditorWindow
     {
+        Dialog selectedDialog = null;
+
         [MenuItem("Window/Dialog Editor")]
         public static void ShowEditorWindow()
         {
             GetWindow(typeof(DialogEditor), false, "Dialog Editor");
         }
 
-        [OnOpenAssetAttribute(2)]
+        [OnOpenAsset(2)]
         public static bool OnOpenAsset(int instanceId, int line )
         {
             Dialog editor = EditorUtility.InstanceIDToObject(instanceId) as Dialog;
@@ -23,6 +25,33 @@ namespace Dialog.Editor
                 ShowEditorWindow();
             }
             return true;
+        }
+
+        private void OnEnable()
+        {
+            Selection.selectionChanged += OnSelectionChanged;
+        }
+
+
+        private void OnSelectionChanged()
+        {
+            Dialog newDialogue = Selection.activeObject as Dialog;
+            if (newDialogue != null)
+            {
+                selectedDialog = newDialogue;
+                Repaint();
+            }
+        }
+
+        private void OnGUI()
+        {
+           if(selectedDialog == null)
+            {
+               EditorGUILayout.LabelField("No Dialog Selected");
+            }else
+            {
+                EditorGUILayout.LabelField(selectedDialog.name);
+            }
         }
     }
 }
